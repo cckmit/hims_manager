c9db06ee17ca54ef847306026aed2aa69273c9e8
架构-邬良瑞 <wu_lr@hisuntech.com>
2019-09-05 10:09:38 +0800

    fix：项目文件下载失败

--- service/src/main/java/com/cmpay/lemon/monitor/service/impl/demand/ReqTaskServiceImpl.java
+++ service/src/main/java/com/cmpay/lemon/monitor/service/impl/demand/ReqTaskServiceImpl.java
@@ -130,7 +130,7 @@ public class ReqTaskServiceImpl implements ReqTaskService {
         return demandRspBO;
     }
 
-    private PageInfo<DemandBO>  getPageInfo(DemandBO demandBO) {
+    private PageInfo<DemandBO> getPageInfo(DemandBO demandBO) {
         DemandDO demandDO = new DemandDO();
         BeanConvertUtils.convert(demandDO, demandBO);
         PageInfo<DemandBO> pageInfo = PageUtils.pageQueryWithCount(demandBO.getPageNum(), demandBO.getPageSize(),
@@ -161,7 +161,7 @@ public class ReqTaskServiceImpl implements ReqTaskService {
     private List<DemandDO> reqTask(DemandBO demandBO) {
         DemandDO demandDO = new DemandDO();
         BeanConvertUtils.convert(demandDO, demandBO);
-        return  demandDao.getReqTask(demandDO);
+        return demandDao.getReqTask(demandDO);
     }
 
 
@@ -252,9 +252,9 @@ public class ReqTaskServiceImpl implements ReqTaskService {
     public Boolean checkNumber(String req_no) {
         boolean bool = false;
         String[] reqNo = req_no.split("-");
-        if (reqNo.length == 3){
+        if (reqNo.length == 3) {
             if ((("REQ".equals(reqNo[0]) || "REQJIRA".equals(reqNo[0]))
-                    && reqNo[1].matches("^\\d{8}$") && reqNo[2].matches("^\\d{4}$"))){
+                    && reqNo[1].matches("^\\d{8}$") && reqNo[2].matches("^\\d{4}$"))) {
                 bool = true;
             }
         }
@@ -282,7 +282,7 @@ public class ReqTaskServiceImpl implements ReqTaskService {
      * @param demandBO
      */
     public void setDefaultUser(DemandBO demandBO) {
-        if (StringUtils.isBlank(demandBO.getCreat_user())){
+        if (StringUtils.isBlank(demandBO.getCreat_user())) {
             demandBO.setCreat_user(SecurityUtils.getLoginUserId());
             demandBO.setCreat_time(new Date());
         }
@@ -319,21 +319,21 @@ public class ReqTaskServiceImpl implements ReqTaskService {
     public void checkReqTask(DemandBO demandBO) {
         //判断编号是否规范
         String reqNo = demandBO.getReq_no();
-        if (StringUtils.isNotBlank(reqNo) && !reqTaskService.checkNumber(reqNo)){
+        if (StringUtils.isNotBlank(reqNo) && !reqTaskService.checkNumber(reqNo)) {
             BusinessException.throwBusinessException(MsgEnum.ERROR_REQ_NO);
         }
 
         //如果选择取消或暂停，月初备注不能为空
-        if("30".equals(demandBO.getReq_sts()) && StringUtils.isBlank(demandBO.getMon_remark())
-                ||"40".equals(demandBO.getReq_sts()) && StringUtils.isBlank(demandBO.getMon_remark())){
+        if ("30".equals(demandBO.getReq_sts()) && StringUtils.isBlank(demandBO.getMon_remark())
+                || "40".equals(demandBO.getReq_sts()) && StringUtils.isBlank(demandBO.getMon_remark())) {
             BusinessException.throwBusinessException(MsgEnum.NULL_REMARK);
         }
 
         //主导部门和配合部门不能相同
-        if(StringUtils.isNotBlank(demandBO.getDevp_coor_dept())){
-            String[] devp_coor_dept=demandBO.getDevp_coor_dept().split(",");
-            for (int  i = 0; i < devp_coor_dept.length;i++){
-                if(devp_coor_dept[i].equals(demandBO.getDevp_lead_dept())){
+        if (StringUtils.isNotBlank(demandBO.getDevp_coor_dept())) {
+            String[] devp_coor_dept = demandBO.getDevp_coor_dept().split(",");
+            for (int i = 0; i < devp_coor_dept.length; i++) {
+                if (devp_coor_dept[i].equals(demandBO.getDevp_lead_dept())) {
                     BusinessException.throwBusinessException(MsgEnum.ERROR_DEVP);
                 }
             }
@@ -371,22 +371,22 @@ public class ReqTaskServiceImpl implements ReqTaskService {
         List<DemandDO> insertList = new ArrayList<>();
         List<DemandDO> updateList = new ArrayList<>();
         demandDOS.forEach(m -> {
-            if (StringUtils.isBlank(m.getReq_pro_dept())){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"归属部门不能为空");
+            if (StringUtils.isBlank(m.getReq_pro_dept())) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "归属部门不能为空");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
-            if (StringUtils.isBlank(m.getReq_nm())){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"需求名称不能为空");
+            if (StringUtils.isBlank(m.getReq_nm())) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "需求名称不能为空");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
-            if (StringUtils.isBlank(m.getReq_desc())){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"需求描述不能为空");
+            if (StringUtils.isBlank(m.getReq_desc())) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "需求描述不能为空");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
 
             //判断编号是否规范
             String reqNo = m.getReq_no();
-            if (StringUtils.isNotBlank(reqNo) && !reqTaskService.checkNumber(reqNo)){
+            if (StringUtils.isNotBlank(reqNo) && !reqTaskService.checkNumber(reqNo)) {
                 BusinessException.throwBusinessException(MsgEnum.ERROR_REQ_NO);
             }
 
@@ -394,23 +394,23 @@ public class ReqTaskServiceImpl implements ReqTaskService {
             dictionaryDO.setDic_id("PRD_LINE");
             dictionaryDO.setValue(m.getReq_prd_line());
             List<DictionaryDO> dic = dictionaryDao.getDicByDicId(dictionaryDO);
-            if ( dic.size() == 0 ){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"产品线字典项不存在");
+            if (dic.size() == 0) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "产品线字典项不存在");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
             m.setReq_prd_line(dic.get(0).getName());
 
             dictionaryDO.setUser_name(m.getReq_proposer());
             dic = dictionaryDao.getJdInfo(dictionaryDO);
-            if (dic.size() == 0){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"需求提出人不存在");
+            if (dic.size() == 0) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "需求提出人不存在");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
 
             dictionaryDO.setUser_name(m.getReq_mnger());
             dic = dictionaryDao.getJdInfo(dictionaryDO);
-            if (dic.size() == 0){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"需求负责人不存在");
+            if (dic.size() == 0) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "需求负责人不存在");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
 
@@ -418,11 +418,11 @@ public class ReqTaskServiceImpl implements ReqTaskService {
                 dictionaryDO.setDic_id("REQ_PEROID");
                 dictionaryDO.setValue(m.getPre_cur_period());
                 dic = dictionaryDao.getDicByDicId(dictionaryDO);
-                if ( dic.size() == 0 ){
-                    MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"最新进展字典项不存在");
+                if (dic.size() == 0) {
+                    MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "最新进展字典项不存在");
                     BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
                 }
-               m.setPre_cur_period(dic.get(0).getName());
+                m.setPre_cur_period(dic.get(0).getName());
             }
 
 
@@ -430,27 +430,27 @@ public class ReqTaskServiceImpl implements ReqTaskService {
                 dictionaryDO.setDic_id("REQ_PEROID");
                 dictionaryDO.setValue(m.getPre_mon_period());
                 dic = dictionaryDao.getDicByDicId(dictionaryDO);
-                if ( dic.size() == 0 ){
-                    MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"月初需求阶段字典项不存在");
+                if (dic.size() == 0) {
+                    MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "月初需求阶段字典项不存在");
                     BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
                 }
                 m.setPre_mon_period(dic.get(0).getName());
             }
-            m.setPre_cur_period(StringUtils.isBlank(m.getPre_cur_period())?m.getPre_mon_period():m.getPre_cur_period());
-            m.setPre_mon_period(StringUtils.isBlank(m.getPre_mon_period())?m.getPre_cur_period():m.getPre_mon_period());
+            m.setPre_cur_period(StringUtils.isBlank(m.getPre_cur_period()) ? m.getPre_mon_period() : m.getPre_cur_period());
+            m.setPre_mon_period(StringUtils.isBlank(m.getPre_mon_period()) ? m.getPre_cur_period() : m.getPre_mon_period());
 
             if (StringUtils.isNotBlank(m.getCur_mon_target())) {
                 dictionaryDO.setDic_id("REQ_PEROID");
                 dictionaryDO.setValue(m.getCur_mon_target());
                 dic = dictionaryDao.getDicByDicId(dictionaryDO);
-                if ( dic.size() == 0 ){
-                    MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"本月预计完成阶段字典项不存在");
+                if (dic.size() == 0) {
+                    MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "本月预计完成阶段字典项不存在");
                     BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
                 }
                 m.setCur_mon_target(dic.get(0).getName());
             }
 
-            DemandBO tmp= new DemandBO();
+            DemandBO tmp = new DemandBO();
             BeanUtils.copyPropertiesReturnDest(tmp, m);
             setReqSts(tmp);
             setDefaultUser(tmp);
@@ -469,7 +469,7 @@ public class ReqTaskServiceImpl implements ReqTaskService {
                 //插入SVN为否
                 m.setIs_svn_build("否");
                 insertList.add(m);
-            }else {
+            } else {
                 m.setReq_inner_seq(dem.get(0).getReq_inner_seq());
                 //设置默认值
                 m.setReq_start_mon("");
@@ -501,29 +501,49 @@ public class ReqTaskServiceImpl implements ReqTaskService {
         MultipartFile file = request.getFile(FILE);
         response.reset();
         try (OutputStream output = response.getOutputStream();
-             BufferedOutputStream bufferedOutPut = new BufferedOutputStream(output)){
+             BufferedOutputStream bufferedOutPut = new BufferedOutputStream(output)) {
             Map<String, Object> resMap = BatchDown(file);
 
             File srcfile[] = (File[]) resMap.get("srcfile");
-
             //压缩包名称
             String zipPath = "/home/hims/temp/propkg/";
             String zipName = "项目文档_" + DateUtil.date2String(new Date(), "yyyyMMddHHmmss") + ".zip";
-
             //压缩文件
             File zip = new File(zipPath + zipName);
             reqTaskService.ZipFiles(srcfile, zip, true);
-
             response.setHeader(CONTENT_DISPOSITION, "attchement;filename=" + zipName);
             response.setHeader(ACCESS_CONTROL_EXPOSE_HEADERS, CONTENT_DISPOSITION);
+            //告诉浏览器允许所有的域访问
+            //注意 * 不能满足带有cookie的访问,Origin 必须是全匹配
+            //resp.addHeader("Access-Control-Allow-Origin", "*");
+            //解决办法通过获取Origin请求头来动态设置
+            String origin = request.getHeader("Origin");
+            if (StringUtils.isNotBlank(origin)) {
+                response.addHeader("Access-Control-Allow-Origin", origin);
+            }
+            //允许带有cookie访问
+            response.addHeader("Access-Control-Allow-Credentials", "true");
+            //告诉浏览器允许跨域访问的方法
+            response.addHeader("Access-Control-Allow-Methods", "*");
+            //告诉浏览器允许带有Content-Type,header1,header2头的请求访问
+            //resp.addHeader("Access-Control-Allow-Headers", "Content-Type,header1,header2");
+            //设置支持所有的自定义请求头
+            String headers = request.getHeader("Access-Control-Request-Headers");
+            if (StringUtils.isNotBlank(headers)) {
+                response.addHeader("Access-Control-Allow-Headers", headers);
+            }
+            //告诉浏览器缓存OPTIONS预检请求1小时,避免非简单请求每次发送预检请求,提升性能
+            response.addHeader("Access-Control-Max-Age", "3600");
             response.setContentType("application/octet-stream; charset=utf-8");
-
+            System.err.println(response.getHeader(CONTENT_DISPOSITION));
+            System.err.println(response.getHeader(ACCESS_CONTROL_EXPOSE_HEADERS));
             output.write(org.apache.commons.io.FileUtils.readFileToByteArray(zip));
             bufferedOutPut.flush();
 
             // 删除文件
 //            zip.delete();
         } catch (Exception e) {
+            e.printStackTrace();
             BusinessException.throwBusinessException(MsgEnum.BATCH_IMPORT_FAILED);
         }
     }
@@ -533,63 +553,63 @@ public class ReqTaskServiceImpl implements ReqTaskService {
         List<DemandDO> List = new ArrayList<>();
         Map<String, Object> map = new HashMap<>();
         demandDOS.forEach(m -> {
-            if (StringUtils.isBlank(m.getReq_no())){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"需求编号不能为空");
+            if (StringUtils.isBlank(m.getReq_no())) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "需求编号不能为空");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
-            if (StringUtils.isBlank(m.getReq_nm())){
-                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo()+"需求名称不能为空");
+            if (StringUtils.isBlank(m.getReq_nm())) {
+                MsgEnum.ERROR_IMPORT.setMsgInfo(MsgEnum.ERROR_IMPORT.getMsgInfo() + "需求名称不能为空");
                 BusinessException.throwBusinessException(MsgEnum.ERROR_IMPORT);
             }
 
             //判断编号是否规范
             String reqNo = m.getReq_no();
-            if (StringUtils.isNotBlank(reqNo) && !reqTaskService.checkNumber(reqNo)){
+            if (StringUtils.isNotBlank(reqNo) && !reqTaskService.checkNumber(reqNo)) {
                 BusinessException.throwBusinessException(MsgEnum.ERROR_REQ_NO);
             }
 
-            int start = m.getReq_no().indexOf("-")+1;
-            String reqMonth = m.getReq_no().substring(start,start+6);
+            int start = m.getReq_no().indexOf("-") + 1;
+            String reqMonth = m.getReq_no().substring(start, start + 6);
             m.setReq_start_mon(reqMonth);
             List.add(m);
         });
 
         //循环文件目录
         if (List != null) {
-            File srcfile[]=new File[List.size()*5];
+            File srcfile[] = new File[List.size() * 5];
             int num = 0;
             //要压缩的文件
-            for (int i=0;  i < List.size(); i++) {
+            for (int i = 0; i < List.size(); i++) {
                 //需求说明书、技术方案、原子功能点评估表
                 String path = "/home/hims/temp/Projectdoc/" + List.get(i).getReq_start_mon() + "/"
-                        + List.get(i).getReq_no() + "_" + List.get(i).getReq_nm() ;
+                        + List.get(i).getReq_no() + "_" + List.get(i).getReq_nm();
 
                 File file1 = new File(path + "/开发技术文档/");
-                if  (!file1 .exists()  && !file1 .isDirectory()){
-                    file1 .mkdir();
+                if (!file1.exists() && !file1.isDirectory()) {
+                    file1.mkdir();
                 }
                 File[] tempFile1 = file1.listFiles();
-                if(tempFile1==null){
-                    tempFile1= new File[0];
+                if (tempFile1 == null) {
+                    tempFile1 = new File[0];
                 }
-                for(int j = 0; j < tempFile1.length; j++){
-                    if(tempFile1[j].getName().contains("原子功能点评估表(电子工单)") || tempFile1[j].getName().contains("技术方案说明书")){
-                        srcfile[num]=new File(path + "/开发技术文档/" + tempFile1[j].getName());
+                for (int j = 0; j < tempFile1.length; j++) {
+                    if (tempFile1[j].getName().contains("原子功能点评估表(电子工单)") || tempFile1[j].getName().contains("技术方案说明书")) {
+                        srcfile[num] = new File(path + "/开发技术文档/" + tempFile1[j].getName());
                         num++;
                     }
                 }
 
                 File file2 = new File(path + "/产品文档/");
-                if  (!file2 .exists()  && !file2 .isDirectory()){
-                    file2 .mkdir();
+                if (!file2.exists() && !file2.isDirectory()) {
+                    file2.mkdir();
                 }
                 File[] tempFile2 = file2.listFiles();
-                if(tempFile2==null){
-                    tempFile2= new File[0];
+                if (tempFile2 == null) {
+                    tempFile2 = new File[0];
                 }
-                for(int j = 0; j < tempFile2.length; j++){
-                    if(tempFile2[j].getName().contains("需求方案说明书")){
-                        srcfile[num]=new File(path + "/产品文档/"+ tempFile2[j].getName());
+                for (int j = 0; j < tempFile2.length; j++) {
+                    if (tempFile2[j].getName().contains("需求方案说明书")) {
+                        srcfile[num] = new File(path + "/产品文档/" + tempFile2[j].getName());
                         num++;
                     }
                 }
@@ -602,29 +622,29 @@ public class ReqTaskServiceImpl implements ReqTaskService {
     }
 
     /**
-      文件压缩
+     *  文件压缩
      */
-    public String ZipFiles(File[] srcfile, File zipfile, boolean flag){
+    public String ZipFiles(File[] srcfile, File zipfile, boolean flag) {
         try {
-            byte[] buf=new byte[1024];
+            byte[] buf = new byte[1024];
             FileOutputStream fileOutputStream = new FileOutputStream(zipfile);
             CheckedOutputStream cos = new CheckedOutputStream(fileOutputStream, new CRC32());
             ZipOutputStream out = new ZipOutputStream(cos);
 
-            for (int i=0; i < srcfile.length; i++) {
+            for (int i = 0; i < srcfile.length; i++) {
                 if (srcfile[i] != null) {
-                    FileInputStream in=new FileInputStream(srcfile[i]);
-                    if(flag){
-                        String demandName = srcfile[i].getPath().substring(34,srcfile[i].getPath().length());
-                        String name = demandName.substring(0,demandName.indexOf("/"));
-                        String  path = demandName.substring(demandName.lastIndexOf("/")+1);
-                        out.putNextEntry(new ZipEntry(name+"/"+path));
-                    }else{
+                    FileInputStream in = new FileInputStream(srcfile[i]);
+                    if (flag) {
+                        String demandName = srcfile[i].getPath().substring(34, srcfile[i].getPath().length());
+                        String name = demandName.substring(0, demandName.indexOf("\\"));
+                        String path = demandName.substring(demandName.lastIndexOf("\\") + 1);
+                        out.putNextEntry(new ZipEntry(name + "\\" + path));
+                    } else {
                         out.putNextEntry(new ZipEntry(srcfile[i].getPath()));
                     }
 
                     int len;
-                    while ((len=in.read(buf)) > 0) {
+                    while ((len = in.read(buf)) > 0) {
                         out.write(buf, 0, len);
                     }
                     out.closeEntry();
@@ -633,6 +653,7 @@ public class ReqTaskServiceImpl implements ReqTaskService {
             }
             out.close();
         } catch (IOException e) {
+            e.printStackTrace();
             BusinessException.throwBusinessException(MsgEnum.BATCH_IMPORT_FAILED);
         }
 
