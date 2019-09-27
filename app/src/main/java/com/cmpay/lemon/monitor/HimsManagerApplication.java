fe5b234605bb67573533a67b54949a0aa82e0eef
架构-邬良瑞 <wu_lr@hisuntech.com>
2019-09-27 10:54:25 +0800

    fix：添加创建项目同步jira功能

--- app/src/main/java/com/cmpay/lemon/monitor/HimsManagerApplication.java
+++ app/src/main/java/com/cmpay/lemon/monitor/HimsManagerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
  * @author: zhou_xiong
  */
 @LemonBootApplication(exclude = {RabbitAutoConfiguration.class},value = {"com.cmpay"})
-public class MonitorApplication {
+public class HimsManagerApplication {
     public static void main(String[] args) {
-        LemonFramework.run(MonitorApplication.class, args);
+        LemonFramework.run(HimsManagerApplication.class, args);
     }
 }
