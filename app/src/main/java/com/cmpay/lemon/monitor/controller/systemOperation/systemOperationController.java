24d0a8d2660a67bdcaa1154bf481769dfb22b747
架构-邬良瑞 <wu_lr@hisuntech.com>
2019-11-06 09:32:14 +0800

    fix：系统操作

---
+++ app/src/main/java/com/cmpay/lemon/monitor/controller/systemOperation/systemOperationController.java
@@ -0,0 +1,13 @@
+package com.cmpay.lemon.monitor.controller.systemOperation;
+
+
+import com.cmpay.lemon.monitor.constant.MonitorConstants;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = MonitorConstants.SYSTEMOPERATION_PATH)
+public class systemOperationController {
+
+
+}
